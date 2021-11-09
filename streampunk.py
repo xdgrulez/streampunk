56175d42ea6d0bb9359d7c46d09f7fdf2449a14a
Ralph Debusmann <ralph.debusmann@de.bosch.com>
2021-11-09 11:29:20 +0100

    simplified py() function

--- streampunk.py
+++ streampunk.py
@@ -14,12 +14,19 @@ import org.xdgrulez.streampunk.addon.Replicate as Replicate
 import org.xdgrulez.streampunk.addon.SchemaRegistry as SchemaRegistry
 
 def py(object):
-    if isinstance(object, java.type("java.util.HashMap")):
+    def is_sp_rec_object(object):
+        return hasattr(object, "getClass") and hasattr(object.getClass(), "getName") and object.getClass().getName().startswith("org.xdgrulez.streampunk.record.")
+
+    def method_str_to_key_str(method_str):
+        return method_str[3].lower() + method_str[4:]
+
+    if isinstance(object, java.type("java.util.ArrayList")):
+        return [py(list_element_object) for list_element_object in object]
+    elif isinstance(object, java.type("java.util.HashMap")):
         key_str_list = object.keySet().toArray()
-        return { key_str: py(object.get(key_str)) for key_str in key_str_list }
-    elif isinstance(object, java.type("org.xdgrulez.streampunk.record.OffsetsRec")):
-        earliest_dict = py(object.getEarliest())
-        latest_dict = py(object.getEarliest())
-        return { "earliest": earliest_dict, "latest": latest_dict }
+        return {key_str: py(object.get(key_str)) for key_str in key_str_list}
+    elif is_sp_rec_object(object):
+        getter_str_key_str_tuple_list = [(method_str, method_str_to_key_str(method_str)) for method_str in dir(object) if method_str.startswith("get")]
+        return {key_str: py(getattr(object, method_str)()) for (method_str, key_str) in getter_str_key_str_tuple_list}
     else:
         return object
