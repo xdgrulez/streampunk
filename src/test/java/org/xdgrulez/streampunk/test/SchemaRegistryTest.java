e3ca5efa66b06de3ef6ac7c1994c86bb5c1af674
Ralph M. Debusmann <matthias.debusmann@gmail.com>
2022-05-24 11:29:21 +0200

    added tests to show how to deal with Avro

--- src/test/java/org/xdgrulez/streampunk/test/SchemaRegistryTest.java
+++ src/test/java/org/xdgrulez/streampunk/test/SchemaRegistryTest.java
@@ -2,14 +2,24 @@ package org.xdgrulez.streampunk.test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.github.javafaker.Faker;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xdgrulez.streampunk.admin.Topic;
+import org.xdgrulez.streampunk.consumer.Consumer;
+import org.xdgrulez.streampunk.consumer.ConsumerStringAvro;
+import org.xdgrulez.streampunk.producer.ProducerStringAvro;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 
 public class SchemaRegistryTest {
@@ -21,7 +31,58 @@ public class SchemaRegistryTest {
     public void tearDown() {
     }
 
-        //    @Test
+    protected void produceAvro(String clusterString, String topicString) throws InterruptedException {
+        // Create topic
+        if (Topic.exists(clusterString, topicString)) {
+            Topic.delete(clusterString, topicString, false);
+            Thread.sleep(1000);
+        }
+        Topic.create(clusterString, topicString, 1, 1, null);
+
+        // Create Avro schema
+        var addressSchema = "{\"type\":\"record\"," +
+                "\"name\":\"address\"," +
+                "\"fields\":["  +
+                "{\"name\":\"streetName\",\"type\":\"string\"}," +
+                "{\"name\":\"buildingNumber\",\"type\":\"string\"}," +
+                "{\"name\":\"city\",\"type\":\"string\"}," +
+                "{\"name\":\"country\",\"type\":\"string\"}]}";
+        var parser = new Schema.Parser();
+        var schema = parser.parse(addressSchema);
+
+        // Produce a couple of example messages
+        for (int i = 0; i < 10; i++) {
+            var genericRecord = new GenericData.Record(schema);
+            var faker = new Faker();
+            genericRecord.put("streetName", faker.address().streetName());
+            genericRecord.put("buildingNumber", faker.address().buildingNumber());
+            genericRecord.put("city", faker.address().city());
+            genericRecord.put("country", faker.address().country());
+
+            ProducerStringAvro.produce(clusterString, topicString, null, genericRecord);
+        }
+    }
+
+//    @Test
+    public void testProduceAvro() throws InterruptedException {
+        var clusterString = "karapace";
+        var topicString = "addresses.avro";
+        produceAvro(clusterString, topicString);
+    }
+
+//    @Test
+    public void testProduceAndConsumeAvro() throws InterruptedException {
+        var clusterString = "karapace";
+        var topicString = "addresses.avro";
+        produceAvro(clusterString, topicString);
+
+        var groupString = "test";
+        ConsumerStringAvro.consume(clusterString, topicString, groupString, Map.of(0, 0L), Map.of(0, 9L),
+                consumerRecord -> System.out.println(consumerRecord), null,
+                Consumer.NON_INTERACTIVE_MAX_POLL_RECORDS, Consumer.NON_INTERACTIVE_MAX_RETRIES, false, 1);
+    }
+
+    //    @Test
     public void testProduceJSONSchema() throws JsonProcessingException {
         var properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
